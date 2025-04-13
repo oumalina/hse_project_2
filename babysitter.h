41e374508acbcdc8c42a9fb64ea579fe0382f311
vvpiker <vladi2029.er@gmail.com>
2025-04-13 13:40:42 +0300

    babysitter.h: added updateGeneral() method

--- babysitter.h
+++ babysitter.h
@@ -2,9 +2,20 @@
 #include <string>
 #include "role.h"
 
-class babysitter : public Role
+class GeneralInformer;
+
+class babysitter : public GeneralObserver, public Role
 {
+private:
+    GeneralInformer* geninf;
 public:
     virtual void work();
     void feed();
+
+    void updateGeneral(int enemy_cx, int enemy_cy, int baby_cx, int baby_cy);
 };
+
+void babysitter::updateGeneral(int enemy_cx, int enemy_cy, int baby_cx, int baby_cy) {
+    baby_coord_x = baby_cx;
+    enemy_coord_y = baby_cy;
+}
