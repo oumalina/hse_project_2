163e58a6b92f6ea8f0971f1e341ea5c8815fb5bc
vvpiker <vladi2029.er@gmail.com>
2025-04-16 17:40:20 +0300

    main.cpp: year and loop counters

--- main.cpp
+++ main.cpp
@@ -3,9 +3,17 @@
 
 int main()
 {
-    while (1)
+    bool flag = true;
+    int counter = 0; 
+    int year = 0;
+    const int LOOPS_PER_YEAR = 4;
+    while (flag)
     {
-        
+
+        counter++;
+        if (counter % LOOPS_PER_YEAR == 0) {
+            year++;
+        }
     }
 
     return 0;
