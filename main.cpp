ddf585aa83a5b4d32f56476b5cef5a0a551d1327
vvpiker <vladi2029.er@gmail.com>
2025-04-16 17:41:57 +0300

    add sleep function

--- main.cpp
+++ main.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 #include <time.h>
 
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif // _WIN32
+
+using namespace std;
+
+void sleepcp(int milliseconds);
+
+void sleepcp(int milliseconds) // Cross-platform sleep function
+{
+    #ifdef _WIN32
+        Sleep(milliseconds);
+    #else
+        usleep(milliseconds * 1000);
+    #endif // _WIN32
+}
+
 int main()
 {
     bool flag = true;
-    int counter = 0; 
+    int loop = 0; 
     int year = 0;
     const int LOOPS_PER_YEAR = 4;
+    const int SLEEP_FOR_MS = 250;
     while (flag)
     {
 
-        counter++;
-        if (counter % LOOPS_PER_YEAR == 0) {
+        loop++;
+        if (loop % LOOPS_PER_YEAR == 0) {
             year++;
         }
+        usleep(SLEEP_FOR_MS);
     }
 
     return 0;
